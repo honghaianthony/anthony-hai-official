cbe54ee969b4870e7993ca1029c15f50b87c5000
Anthony Hai <namsaoquan@gmail.com>
2022-01-30 00:02:53 +0700

    Almost done navbar and a little in homepage

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,10 +1,62 @@
 import React, { useState, useEffect } from "react";
-
+import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./Navbar.scss";
 
 function Navbar() {
-    return <>Hello</>;
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light ">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <img src={logo} alt="" className="anthonyhai-logo" />
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link active"
+                                aria-current="page"
+                                to="#"
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="#">
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="#">
+                                Project
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="#">
+                                Blog
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="#">
+                                Connection
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
