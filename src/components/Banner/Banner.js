b37013ddc0c27d4c2758a2856513a30e77df7751
Anthony Hai <namsaoquan@gmail.com>
2022-01-30 23:56:11 +0700

    Fix something

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,12 +5,27 @@ import avatar from "../../assets/images/avatar-round.png";
 function Banner() {
     return (
         <div className="banner-info">
-            <div className="avatar">
-                <img src={avatar} alt="" />
-            </div>
-            <div className="information">
-                <div className="info-name"></div>
-                <div className="social-links"></div>
+            <div className="banner-info-content">
+                <div className="container">
+                    <div className="row">
+                        <div className="info-left">
+                            <div className="avatar">
+                                <img src={avatar} alt="" />
+                            </div>
+                        </div>
+                        <div className="info-right">
+                            <div className="information">
+                                <div className="info-name">
+                                    Dang Ngo Hong Hai
+                                </div>
+                                <div className="sub-info-name">
+                                    become IT Student at 18, now 21
+                                </div>
+                                <div className="social-links"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
