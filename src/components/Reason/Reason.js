07264a8cafd077be357bdf8973258dd7a58f1fbb
Anthony Hai <namsaoquan@gmail.com>
2022-02-02 23:25:43 +0700

    Add new files

---
+++ src/components/Reason/Reason.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Reason.scss";
+
+function Reason() {
+    var index = 0;
+
+    function imageSlider() {}
+    return <div></div>;
+}
+
+export default Reason;
