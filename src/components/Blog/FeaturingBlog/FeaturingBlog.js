3946550207ffff15ea5d970e4b6331254016a587
Anthony Hai <namsaoquan@gmail.com>
2022-02-11 00:08:54 +0700

    Almost done blog outline

--- src/components/Blog/FeaturingBlog/FeaturingBlog.js
+++ src/components/Blog/FeaturingBlog/FeaturingBlog.js
@@ -1,46 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./FeaturingBlog.scss";
-import DCE from "../../../assets/images/dce470.png";
+import DCE from "../../../assets/images/dce.png";
 function FeaturingBlog(props) {
     return (
         <>
-            <div className="featuring-blog-container">
-                <div className="featuring-blog-title">
-                    <h4>Featuring blog</h4>
-                </div>
-                <Link className="featuring-blog-link" to="/">
-                    <div className="featuring-blog-detail-container">
-                        <figure
-                            className="blog-item-wrap"
-                            data-category={props.label}
+            <div className="blog-item">
+                <Link className="blog-item-link" to="#">
+                    <figure
+                        className="blog-item-wrap"
+                        data-category={props.label}
+                    >
+                        <img
+                            className="blog-item-img"
+                            alt={props.title}
+                            src={DCE}
+                        />
+                    </figure>
+                    <div className="blog-item-content">
+                        <div
+                            className="blog-item-info"
+                            data-category={props.info}
                         >
-                            <img
-                                className="blog-item-img"
-                                alt={props.title}
-                                src={DCE}
-                            />
-                        </figure>
-                        <div className="featuring-blog-detail-right">
-                            <div className="title-blog">
-                                <h4>New Blog</h4>
-                                <span class="animate-border mt-2 mb-6"></span>
-                            </div>
-                            <div className="content-in-sum-blog">
-                                <p>This is the content of blog</p>
-                            </div>
-                            <span class="animate-border-second"></span>
-                            <div className="info-blog">
-                                <div className="author-blog">
-                                    <p>
-                                        Author:{" "}
-                                        <Link to="/">Dang Ngo Hong Hai</Link>
-                                    </p>
+                            <h5 className="blog-item-title">Title</h5>
+
+                            <p className="blog-item-description">
+                                ContentContentContent
+                            </p>
+                        </div>
+                        <div className="blog-item-author">
+                            <div className="blog-text-left">
+                                <div className="blog-author-name-container">
+                                    <span>Author:</span>
+                                    <span className="blog-author-name">
+                                        <Link
+                                            to="/"
+                                            className="author-link-info"
+                                        >
+                                            Dang Ngo Hong Hai
+                                        </Link>
+                                    </span>
                                 </div>
-                                <div className="date-create-blog">
-                                    {" "}
-                                    <span class="animate-border-third"></span>
-                                    25/01/2022
+
+                                <div className="blog-time-container">
+                                    <i className="blog-time-icon"></i>
+                                    <span className="blog-time">
+                                        10/02/2022
+                                    </span>
                                 </div>
                             </div>
                         </div>
