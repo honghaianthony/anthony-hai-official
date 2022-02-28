1bc7883034eb0c7d2772eef17e50a1e95be10011
Anthony Hai <namsaoquan@gmail.com>
2022-02-28 23:34:34 +0700

    Done blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -16,58 +16,54 @@ function Blog() {
         setFilteredData(blogs);
     }, [blogs]);
 
-    const idArray = blogs.map((item, index) => {
-        return index;
-    });
     let firstItem = 0;
     let trendBlogs = [1, 2, 3];
-    idArray.splice(0, 4);
 
     const anotherBlog = () => {
-        return trendBlogs.map((item) => {
-            if (filteredData.length > 0) {
-                return filteredData.map((item, index) => {
-                    return (
-                        <FeaturingBlog
-                            key={index}
-                            src={item.coverImage}
-                            title={item.title}
-                            description={item.description}
-                            author={`${item.User.firstName} ${item.User.lastName}`}
-                            time={new Date(item.updatedAt).toLocaleDateString()}
-                        />
-                    );
-                });
-            }
+        return filteredData.map((item, index) => {
+            let path = "/blog/" + item.id;
+            return (
+                <FeaturingBlog
+                    key={index}
+                    src={item.coverImage}
+                    title={item.title}
+                    author={`${item.User.firstName} ${item.User.lastName}`}
+                    time={new Date(item.updatedAt).toLocaleDateString()}
+                    path={path}
+                    description={item.description}
+                />
+            );
         });
     };
 
     return (
         <div className="blog-container">
             {blogs.length > 0 ? (
-                <div className="featuring-blog">
-                    <h4 className="featuring-blog-title">Featuring Blog</h4>
-                    <div className="featuring-blog-item">
-                        <FeaturingBlog
-                            src={blogs[firstItem].coverImage}
-                            title={blogs[firstItem].title}
-                            description={blogs[firstItem].description}
-                            author={`${blogs[firstItem].User.firstName} ${blogs[firstItem].User.lastName} `}
-                            time={new Date(
-                                blogs[firstItem].updatedAt
-                            ).toLocaleDateString()}
-                            //view='134'
-                            path={`/blogs/${blogs[firstItem].id}`}
-                        />
+                <div className="blog-list">
+                    <div className="featuring-blog">
+                        <h4 className="featuring-blog-title">Featuring Blog</h4>
+                        <div className="featuring-blog-item">
+                            <FeaturingBlog
+                                src={blogs[firstItem].coverImage}
+                                title={blogs[firstItem].title}
+                                description={blogs[firstItem].description}
+                                author={`${blogs[firstItem].User.firstName} ${blogs[firstItem].User.lastName} `}
+                                time={new Date(
+                                    blogs[firstItem].updatedAt
+                                ).toLocaleDateString()}
+                                //view='134'
+                                path={`/blogs/${blogs[firstItem].id}`}
+                            />
+                        </div>
+                    </div>
+                    <div className="another-blog">
+                        <h4 className="another-blog-title">Another Blogs</h4>
+                        <div className="another-blog-item">{anotherBlog()}</div>
                     </div>
                 </div>
             ) : (
                 <div className="loader"></div>
             )}
-            <div className="another-blog">
-                <h4 className="another-blog-title">Another Blogs</h4>
-                <div className="another-blog-item">{anotherBlog()}</div>
-            </div>
         </div>
     );
 }
