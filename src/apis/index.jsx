bc24cefb90b8d8612c1fddc1183e1c86bc6b0389
Anthony Hai <namsaoquan@gmail.com>
2022-03-13 13:41:42 +0700

    Fix data

--- src/apis/index.jsx
+++ src/apis/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3301/api",
+    baseURL: "https://arcane-ocean-27301.herokuapp.com/api",
 });
 
 axiosInstance.defaults.headers.common[
