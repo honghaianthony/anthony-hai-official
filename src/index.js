761399848594924f97234039f1473d5a276522a2
Anthony Hai <namsaoquan@gmail.com>
2022-03-14 15:52:24 +0700

    Fix

--- src/index.js
+++ src/index.js
@@ -5,37 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import AuthProvider from "./store/Provider";
-import { hydrate, render } from "react-dom";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate(
-        <React.StrictMode>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </React.StrictMode>,
-        rootElement
-    );
-} else {
-    render(
-        <React.StrictMode>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </React.StrictMode>,
-        rootElement
-    );
-}
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <AuthProvider>
-//             <App />
-//         </AuthProvider>
-//     </React.StrictMode>,
-//     document.getElementById("root")
-// );
+ReactDOM.render(
+    <React.StrictMode>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
