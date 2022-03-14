7b60dff9b4b0746acb97a45ed7cf5f70b95bc716
Anthony Hai <namsaoquan@gmail.com>
2022-03-14 14:53:55 +0700

    Add react snap

--- src/index.js
+++ src/index.js
@@ -5,15 +5,33 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import AuthProvider from "./store/Provider";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-    <React.StrictMode>
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+    hydrate(
         <AuthProvider>
             <App />
-        </AuthProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+        </AuthProvider>,
+        rootElement
+    );
+} else {
+    render(
+        <AuthProvider>
+            <App />
+        </AuthProvider>,
+        rootElement
+    );
+}
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <AuthProvider>
+//             <App />
+//         </AuthProvider>
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
