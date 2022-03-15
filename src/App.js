482b65182bf79370196630018a81a77732fba7c2
Anthony Hai <namsaoquan@gmail.com>
2022-03-15 18:45:54 +0700

    Add meta tags name

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
                         name="keywords"
                         content="Portfolio, Hong Hai Official, Hong Hai Portfolio, Hong Hai Blog, Anthony Hai Official, anthonyhai, Anthony Hai Portfolio"
                     />
+
                     <style type="text/css">{`
                 .app{
                     background-color: #fff;
@@ -26,6 +27,41 @@ function App() {
                     background-color: #f8f9fa;
                     color: black;
                 }`}</style>
+                    <meta name="title" content="Hong Hai Official" />
+                    <meta
+                        name="description"
+                        content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
+                    />
+
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:url"
+                        content="https://anthony-hai-official.vercel.app/"
+                    />
+                    <meta property="og:title" content="Hong Hai Official" />
+                    <meta
+                        property="og:description"
+                        content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
+                    />
+                    <meta property="og:image" content="" />
+
+                    <meta
+                        property="twitter:card"
+                        content="summary_large_image"
+                    />
+                    <meta
+                        property="twitter:url"
+                        content="https://anthony-hai-official.vercel.app/"
+                    />
+                    <meta
+                        property="twitter:title"
+                        content="Hong Hai Official"
+                    />
+                    <meta
+                        property="twitter:description"
+                        content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
+                    />
+                    <meta property="twitter:image" content=""></meta>
                 </Helmet>
                 <div className="App">
                     <Router />
