b1abda8b65fbad9b23415dc4d2e26507b9fe3c68
Anthony Hai <namsaoquan@gmail.com>
2022-03-20 15:32:33 +0700

    Remove react-snap

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
 import Router from "./routes";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import "./App.css";
 
 function App() {
     return (
-        <HelmetProvider>
-            <Helmet>
-                <title>Hong Hai Official</title>
-                <meta
-                    name="keywords"
-                    content="Portfolio, Hong Hai Official, Hong Hai Portfolio, Hong Hai Blog, Anthony Hai Official, anthonyhai, Anthony Hai Portfolio"
-                />
-
-                <style type="text/css">{`
-                .app{
-                    background-color: #fff;
-                }
-                .header{
-                    background-color: #f8f9fa;
-                    color: black;
-                }`}</style>
-                <meta name="title" content="Hong Hai Official" />
-                <meta
-                    name="description"
-                    content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
-                />
-
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://www.anthonyhai.tk/" />
-                <meta property="og:title" content="Hong Hai Official" />
-                <meta
-                    property="og:description"
-                    content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
-                />
-                <meta property="og:image" content="" />
-
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta
-                    property="twitter:url"
-                    content="https://www.anthonyhai.tk/"
-                />
-                <meta property="twitter:title" content="Hong Hai Official" />
-                <meta
-                    property="twitter:description"
-                    content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
-                />
-                <meta property="twitter:image" content=""></meta>
-            </Helmet>
-            <div className="App">
-                <Router />
-            </div>
-        </HelmetProvider>
+        <div className="App">
+            <Router />
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,24 +5,70 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import AuthProvider from "./store/Provider";
-import { hydrate, render } from "react-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate(
+ReactDOM.hydrate(
+    <HelmetProvider>
         <AuthProvider>
-            <App />
-        </AuthProvider>,
-        rootElement
-    );
-} else {
-    render(
-        <AuthProvider>
-            <App />
-        </AuthProvider>,
-        rootElement
-    );
-}
+            <App>
+                <Helmet>
+                    <title>Hong Hai Official</title>
+                    <meta
+                        name="keywords"
+                        content="Portfolio, Hong Hai Official, Hong Hai Portfolio, Hong Hai Blog, Anthony Hai Official, anthonyhai, Anthony Hai Portfolio"
+                    />
+
+                    <style type="text/css">{`
+                .app{
+                    background-color: #fff;
+                }
+                .header{
+                    background-color: #f8f9fa;
+                    color: black;
+                }`}</style>
+                    <meta name="title" content="Hong Hai Official" />
+                    <meta
+                        name="description"
+                        content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
+                    />
+
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:url"
+                        content="https://www.anthonyhai.tk/"
+                    />
+                    <meta property="og:title" content="Hong Hai Official" />
+                    <meta
+                        property="og:description"
+                        content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
+                    />
+                    <meta property="og:image" content="" />
+
+                    <meta
+                        property="twitter:card"
+                        content="summary_large_image"
+                    />
+                    <meta
+                        property="twitter:url"
+                        content="https://www.anthonyhai.tk/"
+                    />
+                    <meta
+                        property="twitter:title"
+                        content="Hong Hai Official"
+                    />
+                    <meta
+                        property="twitter:description"
+                        content="This is the portfolio website of Hong Hai. You can take a look at my portfolio and see how you contact me"
+                    />
+                    <meta property="twitter:image" content=""></meta>
+                    <link rel="canonical" href="https://www.anthonyhai.tk/" />
+                </Helmet>
+            </App>
+        </AuthProvider>
+    </HelmetProvider>,
+    document.getElementById("root")
+);
+
 // ReactDOM.render(
 //     <React.StrictMode>
 //         <AuthProvider>
