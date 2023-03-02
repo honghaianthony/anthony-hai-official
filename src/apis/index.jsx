e27df188e27ff7a07a74e570adde900326a78648
Anthony Hai <namsaoquan@gmail.com>
2023-03-02 11:03:04 +0700

    add new docker for website

--- src/apis/index.jsx
+++ src/apis/index.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: 'https://anthony-hai-back-end-official.vercel.app/api',
-	// baseURL: "http://localhost:3301/api",
+	// baseURL: 'https://anthony-hai-back-end-official.vercel.app/api',
+	baseURL: 'http://localhost:3301/api',
 });
 
 axiosInstance.defaults.headers.common[
