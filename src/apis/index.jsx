215c7ac46b9a034259e95efce64d056f58264f7a
Anthony Hai <namsaoquan@gmail.com>
2023-03-03 13:19:41 +0700

    update: docker compose fe and be

--- src/apis/index.jsx
+++ src/apis/index.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 	// baseURL: 'https://anthony-hai-back-end-official.vercel.app/api',
-	baseURL: 'http://localhost:3301/api',
+	// baseURL: 'http://localhost:3301/api',
+	baseURL: '/',
 });
 
 axiosInstance.defaults.headers.common[
